ac4bc45ee7addf5be0e531f83021fc6d90bc0055
sachaMorin <morin.sacha@gmail.com>
2024-04-04 18:11:48 -0400

    Patch output_ids verification

--- llava/mm_utils.py
+++ llava/mm_utils.py
@@ -65,7 +65,9 @@ class KeywordsStoppingCriteria(StoppingCriteria):
         offset = min(output_ids.shape[1] - self.start_len, 3)
         self.keyword_ids = [keyword_id.to(output_ids.device) for keyword_id in self.keyword_ids]
         for keyword_id in self.keyword_ids:
-            if output_ids[0, -keyword_id.shape[0]:] == keyword_id:
+            # if output_ids[0, -keyword_id.shape[0]:] == keyword_id:
+            #     return True
+            if torch.equal(output_ids[0, -keyword_id.shape[0]:], keyword_id):
                 return True
         outputs = self.tokenizer.batch_decode(output_ids[:, -offset:], skip_special_tokens=True)[0]
         for keyword in self.keywords:
